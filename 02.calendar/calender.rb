411d2e0b6786418b3f4b552f670c580aa461f50f
yushi <yushi20000807@gmail.com>
2024-06-11 22:35:04 +0900

    new file

---
+++ 02.calendar/calender.rb
@@ -0,0 +1,59 @@
+#! /usr/bin/env ruby
+require 'optparse'
+require 'date'
+
+class Calender
+  def initialize(year,month)
+    @year = year
+    @month = month
+  end
+
+  def print
+    puts print_dayandmonth
+    puts print_oneweek
+    print_onemonth
+  end
+
+  def print_dayandmonth
+    "#{@month}月 #{@year}".center(20)
+  end
+
+  def print_oneweek
+    oneweek_jp = "日 月 火 水 木 金 土"
+    return oneweek_jp
+  end
+
+  def print_onemonth
+    week = []
+    #開始曜日を調べる
+    start_day = Date.new(@year,@month,1).wday
+    #開始曜日に応じて、空白を追加する
+    start_day.times{ week << " ".rjust(2) }
+    #最終日を取得する
+    date = Date.new(@year, @month, -1).day
+    #最初の日から最後の日までを文字列配列にする。
+    month_array = (1..date).to_a
+
+    #一日づつ、weekに渡す。
+    month_array.each do |day|
+      week << day.to_s.rjust(2)
+      if week.size == 7 || day == date
+        # week.join(' ')は、配列内の要素をスペースで区切って1つの文字列に連結するメソッド。
+        # たとえば、weekが["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]のような配列であれば、
+        # week.join(' ')は"Mon Tue Wed Thu Fri Sat Sun"という文字列を返す。
+        puts week.join(' ')
+        week.clear
+      end
+    end
+  end
+end
+
+#今日の日付、西暦を取得する。
+options = {:year => Date.today.year,:month => Date.today.month }  # デフォルトで今日の月を設定
+opt = OptionParser.new
+opt.on('-y VAL') {|v| options[:year] = v.to_i}
+opt.on('-m VAL' ) {|v| options[:month] = v.to_i}
+opt.parse!(ARGV)
+
+calender = Calender.new(options[:year],options[:month]);
+calender.print
