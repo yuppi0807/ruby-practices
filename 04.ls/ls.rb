8d80d5d61af1f850d22c08f5a2cef7a0ed7f5b4e
yushi <yushi20000807@gmail.com>
2025-09-23 17:08:33 +0900

    レビュー指摘対応

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
-COL_COUNT = 3
+COL_COUNT = 4
 
 def main
   entry_names = Dir.glob('*')
   max_width = entry_names.map(&:size).max
-  shape_entry_names = shape_entry_names(entry_names)
-  puts_and_shape_width(shape_entry_names, max_width)
+  shape_entry_names = arrange_entry_names(entry_names)
+  puts_like_ls(shape_entry_names, max_width)
 end
 
-def shape_entry_names(entry_names)
-  slice_size = (entry_names.size / COL_COUNT.to_f).ceil
-
-  entry_names = entry_names.each_slice(slice_size).to_a
-  shape_entry_names = arrange_size(entry_names, entry_names[0].size)
+def arrange_entry_names(entry_names)
+  slice_size = entry_names.size.ceildiv(COL_COUNT)
+  slice_entry_names = entry_names.each_slice(slice_size).to_a
+  shape_entry_names = arrange_size(slice_entry_names, slice_size)
   shape_entry_names.transpose
 end
 
 def arrange_size(entry_names, size)
-  entry_names.each do |entry_name|
-    entry_name.fill(nil, size...size)
+  shape_entry_names = []
+  entry_names.each do |entry_name_col|
+    shape_entry_names << entry_name_col + [nil] * [0, size - entry_name_col.length].max
   end
-  entry_names
+  shape_entry_names
 end
 
-def puts_and_shape_width(entry_names, width)
-  entry_names.each do |row|
+def puts_like_ls(shape_entry_names, width)
+  shape_entry_names.each do |row|
     row.each do |col|
       print col.to_s.ljust(width + 2)
     end
