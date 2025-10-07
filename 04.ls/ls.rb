73063a3d05db024b87c00c05250f43fba375fec1
yushi <yushi20000807@gmail.com>
2025-10-07 23:57:08 +0900

    不要な複数形を修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,25 +5,25 @@ COL_COUNT = 3
 def main
   entry_names = Dir.glob('*')
   max_width = entry_names.map(&:size).max
-  entry_names_table = change_list_to_table(entry_names)
-  puts_table(entry_names_table, max_width)
+  entry_name_table = change_list_to_table(entry_names)
+  puts_table(entry_name_table, max_width)
 end
 
 def change_list_to_table(entry_names)
   row_size = entry_names.size.ceildiv(COL_COUNT)
-  entry_names_table_blanks = entry_names.each_slice(row_size).to_a
-  entry_names_table = fill_blanks(entry_names_table_blanks, row_size)
-  entry_names_table.transpose
+  entry_name_table_blanks = entry_names.each_slice(row_size).to_a
+  entry_name_table = fill_blanks(entry_name_table_blanks, row_size)
+  entry_name_table.transpose
 end
 
-def fill_blanks(entry_names_table_blanks, row_size)
-  entry_names_table_blanks.map do |entry_names|
+def fill_blanks(entry_name_table_blanks, row_size)
+  entry_name_table_blanks.map do |entry_names|
     entry_names + [nil] * (row_size - entry_names.length)
   end
 end
 
-def puts_table(entry_names_table, max_width)
-  entry_names_table.each do |entry_names|
+def puts_table(entry_name_table, max_width)
+  entry_name_table.each do |entry_names|
     entry_names.each do |entry_name|
       print entry_name.to_s.ljust(max_width + 2)
     end
