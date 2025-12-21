d5ec24a77b734cea9582aa9d08bcbacb50ec75c8
yushi <yushi20000807@gmail.com>
2025-12-22 01:24:47 +0900

    entry_namesを取得する処理を複数回行わないように修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -43,10 +43,9 @@ FILE_INFO_KEYS = %i[
 def main
   success, options = parse_options
   return unless success
-
+  entry_names = Dir.glob('*')
   if options[:l]
-    entry_info_table = create_entry_info_table
-    total_blocks = create_total_blocks
+    total_blocks, entry_info_table = create_entry_info_table(entry_names)
     puts_entry_names_info(total_blocks, entry_info_table)
   else
     max_width = entry_names.map(&:size).max
@@ -88,7 +87,8 @@ def puts_table(entry_name_table, max_width)
   end
 end
 
-def create_entry_info_table
+def create_entry_info_table(entry_names)
+  total_blocks = create_total_blocks(entry_names)
   entry_info_table = entry_names.map do |entry_name|
     entry_info = {}
     stat = File::Stat.new(entry_name)
@@ -102,17 +102,13 @@ def create_entry_info_table
     entry_info[:entry_name] = entry_name
     entry_info
   end
-  align_width(entry_info_table)
+  [total_blocks, align_width(entry_info_table)]
 end
 
-def create_total_blocks
+def create_total_blocks(entry_names)
   entry_names.sum do |entry_name|
     File::Stat.new(entry_name).blocks
   end
-end
-
-def entry_names
-  Dir.glob('*')
 end
 
 def get_file_mode(stat, entry_name)
