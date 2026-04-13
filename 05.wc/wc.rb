51786611c50b1aced4c03af54e2aed1416a40e77
yushi <yushi20000807@gmail.com>
2026-04-13 20:55:46 +0900

    ファイルを開いた時の内容で保持するように修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -14,7 +14,8 @@ def main
   options = parse_options
 
   if $stdin.tty?
-    puts_metadata(options[:non_options], options[:option])
+    files = build_input
+    puts_metadata(options[:non_options], options[:option], files)
   else
     puts_stdindata(options[:non_options], options[:option])
   end
@@ -35,8 +36,26 @@ rescue OptionParser::InvalidOption
   abort '不正なオプションです'
 end
 
-def puts_metadata(non_options, options)
-  keys, metadata_list = make_metadata_list(non_options, options)
+def build_input
+  if ARGV.any?
+    ARGV.map do |path|
+      {
+        filename: path,
+        contents: File.read(path)
+      }
+    end
+  else
+    [
+      {
+        filename: nil,
+        contents: STDIN.read
+      }
+    ]
+  end
+end
+
+def puts_metadata(non_options, options, files)
+  keys, metadata_list = make_metadata_list(non_options, options, files)
   max_widths = make_max_widths(keys, metadata_list)
   metadata_list.each do |metadata|
     cols = keys.map do |key|
@@ -50,20 +69,22 @@ def puts_metadata(non_options, options)
   end
 end
 
-def make_metadata_list(non_options, options)
-  metadata_list = paths.map { |path| file_metadata(path, non_options, options) }
+def make_metadata_list(non_options, options, files)
+  metadata_list = files.map { |file| file_metadata(non_options, options, file) }
   keys = metadata_list[0].keys
   metadata_list << make_total_metadata(keys, metadata_list) if metadata_list.size > 1
 
   [keys, metadata_list]
 end
 
-def file_metadata(path, non_options, options)
+def file_metadata(non_options, options, file)
+  contents = file[:contents]
   metadata = {}
-  metadata[:line] = File.readlines(path).size if options[:l] || non_options
-  metadata[:word] = File.read(path).split.size if options[:w] || non_options
-  metadata[:byte] = File.stat(path).size if options[:c] || non_options
-  metadata[:path] = path
+  metadata[:line] = make_stdin_line(contents) if options[:l] || non_options
+  metadata[:word] = make_stdin_word(contents) if options[:w] || non_options
+  metadata[:byte] = make_stdin_byte(contents) if options[:c] || non_options
+  metadata[:path] = file[:filename]
+
   metadata
 end
 
